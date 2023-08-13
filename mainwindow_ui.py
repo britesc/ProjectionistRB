c8f285a1c5073cc746bcf925e72b5ddb163a134e
Julian Bourne <julian@britesc.com>
2023-08-13 22:07:26 +0200

    Addded mainwindow

---
+++ mainwindow_ui.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QHBoxLayout,
+    QLabel, QMainWindow, QPushButton, QSizePolicy,
+    QStackedWidget, QStatusBar, QTabWidget, QVBoxLayout,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(996, 639)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout_1_Ribbon_Bar = QVBoxLayout()
+        self.verticalLayout_1_Ribbon_Bar.setSpacing(0)
+        self.verticalLayout_1_Ribbon_Bar.setObjectName(u"verticalLayout_1_Ribbon_Bar")
+        self.horizontalLayout_1_Ribbon_Bar = QHBoxLayout()
+        self.horizontalLayout_1_Ribbon_Bar.setSpacing(0)
+        self.horizontalLayout_1_Ribbon_Bar.setObjectName(u"horizontalLayout_1_Ribbon_Bar")
+        self.horizontalLayout_1_Ribbon_Bar.setContentsMargins(-1, 0, -1, 0)
+        self.groupBox_1_Master = QGroupBox(self.centralwidget)
+        self.groupBox_1_Master.setObjectName(u"groupBox_1_Master")
+        self.gridLayoutWidget = QWidget(self.groupBox_1_Master)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(0, 0, 161, 171))
+        self.gridLayout_1_Master = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_1_Master.setSpacing(0)
+        self.gridLayout_1_Master.setObjectName(u"gridLayout_1_Master")
+        self.gridLayout_1_Master.setContentsMargins(0, 0, 0, 0)
+        self.label_Master_R3C0 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R3C0.setObjectName(u"label_Master_R3C0")
+        self.label_Master_R3C0.setMinimumSize(QSize(32, 32))
+        self.label_Master_R3C0.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R3C0, 3, 0, 1, 1)
+
+        self.label_Master_R2C0 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R2C0.setObjectName(u"label_Master_R2C0")
+        self.label_Master_R2C0.setMinimumSize(QSize(32, 32))
+        self.label_Master_R2C0.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R2C0, 2, 0, 1, 1)
+
+        self.label_Master_R0C2 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R0C2.setObjectName(u"label_Master_R0C2")
+        self.label_Master_R0C2.setMinimumSize(QSize(32, 32))
+        self.label_Master_R0C2.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R0C2, 0, 2, 1, 1)
+
+        self.label_Master_R2C1 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R2C1.setObjectName(u"label_Master_R2C1")
+        self.label_Master_R2C1.setMinimumSize(QSize(32, 32))
+        self.label_Master_R2C1.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R2C1, 2, 1, 1, 1)
+
+        self.label_Master_R3C1 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R3C1.setObjectName(u"label_Master_R3C1")
+        self.label_Master_R3C1.setMinimumSize(QSize(32, 32))
+        self.label_Master_R3C1.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R3C1, 3, 1, 1, 1)
+
+        self.label_Master_R1C2 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R1C2.setObjectName(u"label_Master_R1C2")
+        self.label_Master_R1C2.setMinimumSize(QSize(32, 32))
+        self.label_Master_R1C2.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R1C2, 1, 2, 1, 1)
+
+        self.label_Master_R1C3 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R1C3.setObjectName(u"label_Master_R1C3")
+        self.label_Master_R1C3.setMinimumSize(QSize(32, 32))
+        self.label_Master_R1C3.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R1C3, 1, 3, 1, 1)
+
+        self.label_Master_R2C2 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R2C2.setObjectName(u"label_Master_R2C2")
+        self.label_Master_R2C2.setMinimumSize(QSize(32, 32))
+        self.label_Master_R2C2.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R2C2, 2, 2, 1, 1)
+
+        self.label_Master_R2C3 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R2C3.setObjectName(u"label_Master_R2C3")
+        self.label_Master_R2C3.setMinimumSize(QSize(32, 32))
+        self.label_Master_R2C3.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R2C3, 2, 3, 1, 1)
+
+        self.label_Master_R3C2 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R3C2.setObjectName(u"label_Master_R3C2")
+        self.label_Master_R3C2.setMinimumSize(QSize(32, 32))
+        self.label_Master_R3C2.setMaximumSize(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R3C2, 3, 2, 1, 1)
+
+        self.label_Master_R0C0 = QLabel(self.gridLayoutWidget)
+        self.label_Master_R0C0.setObjectName(u"label_Master_R0C0")
+        self.label_Master_R0C0.setMinimumSize(QSize(64, 64))
+        self.label_Master_R0C0.setMaximumSize(QSize(64, 64))
+        self.label_Master_R0C0.setBaseSize(QSize(64, 64))
+        self.label_Master_R0C0.setScaledContents(False)
+        self.label_Master_R0C0.setAlignment(Qt.AlignCenter)
+        self.label_Master_R0C0.setIndent(0)
+
+        self.gridLayout_1_Master.addWidget(self.label_Master_R0C0, 0, 0, 2, 2)
+
+        self.pushButton_Master_R0C3 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_Master_R0C3.setObjectName(u"pushButton_Master_R0C3")
+        self.pushButton_Master_R0C3.setMinimumSize(QSize(32, 32))
+        self.pushButton_Master_R0C3.setMaximumSize(QSize(32, 32))
+        self.pushButton_Master_R0C3.setSizeIncrement(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.pushButton_Master_R0C3, 0, 3, 1, 1)
+
+        self.pushButton_Master_R3C3 = QPushButton(self.gridLayoutWidget)
+        self.pushButton_Master_R3C3.setObjectName(u"pushButton_Master_R3C3")
+        self.pushButton_Master_R3C3.setMinimumSize(QSize(32, 32))
+        self.pushButton_Master_R3C3.setMaximumSize(QSize(32, 32))
+        self.pushButton_Master_R3C3.setSizeIncrement(QSize(32, 32))
+
+        self.gridLayout_1_Master.addWidget(self.pushButton_Master_R3C3, 3, 3, 1, 1)
+
+        self.gridLayout_1_Master.setColumnMinimumWidth(0, 32)
+        self.gridLayout_1_Master.setColumnMinimumWidth(1, 32)
+        self.gridLayout_1_Master.setColumnMinimumWidth(2, 32)
+        self.gridLayout_1_Master.setColumnMinimumWidth(3, 32)
+        self.gridLayout_1_Master.setRowMinimumHeight(0, 32)
+        self.gridLayout_1_Master.setRowMinimumHeight(1, 32)
+        self.gridLayout_1_Master.setRowMinimumHeight(2, 32)
+        self.gridLayout_1_Master.setRowMinimumHeight(3, 32)
+
+        self.horizontalLayout_1_Ribbon_Bar.addWidget(self.groupBox_1_Master)
+
+        self.groupBox_2_Tabs = QGroupBox(self.centralwidget)
+        self.groupBox_2_Tabs.setObjectName(u"groupBox_2_Tabs")
+        self.tabWidget_Ribbon_Bar = QTabWidget(self.groupBox_2_Tabs)
+        self.tabWidget_Ribbon_Bar.setObjectName(u"tabWidget_Ribbon_Bar")
+        self.tabWidget_Ribbon_Bar.setGeometry(QRect(6, -1, 791, 171))
+        self.tabWidget_Ribbon_Bar.setMinimumSize(QSize(0, 140))
+        self.tab_1_Home = QWidget()
+        self.tab_1_Home.setObjectName(u"tab_1_Home")
+        self.tab_1_Home.setMinimumSize(QSize(0, 0))
+        self.groupBox_T1B1 = QGroupBox(self.tab_1_Home)
+        self.groupBox_T1B1.setObjectName(u"groupBox_T1B1")
+        self.groupBox_T1B1.setGeometry(QRect(10, 0, 181, 133))
+        self.groupBox_T1B1.setMinimumSize(QSize(0, 133))
+        self.groupBox_T1B1.setMaximumSize(QSize(16777215, 133))
+        self.groupBox_T1B1.setSizeIncrement(QSize(0, 0))
+        self.groupBox_T1B1.setAlignment(Qt.AlignCenter)
+        self.groupBox_T1B1.setFlat(False)
+        self.groupBox_T1B1.setCheckable(False)
+        self.label_T1B1 = QLabel(self.groupBox_T1B1)
+        self.label_T1B1.setObjectName(u"label_T1B1")
+        self.label_T1B1.setGeometry(QRect(50, 115, 67, 17))
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_T1B1.sizePolicy().hasHeightForWidth())
+        self.label_T1B1.setSizePolicy(sizePolicy)
+        self.label_T1B1.setMaximumSize(QSize(16777215, 16777213))
+        self.groupBox_T1B2 = QGroupBox(self.tab_1_Home)
+        self.groupBox_T1B2.setObjectName(u"groupBox_T1B2")
+        self.groupBox_T1B2.setGeometry(QRect(200, 0, 181, 133))
+        self.groupBox_T1B2.setMinimumSize(QSize(0, 133))
+        self.groupBox_T1B2.setMaximumSize(QSize(16777215, 133))
+        self.groupBox_T1B2.setSizeIncrement(QSize(0, 0))
+        self.groupBox_T1B2.setAlignment(Qt.AlignCenter)
+        self.groupBox_T1B2.setFlat(False)
+        self.groupBox_T1B2.setCheckable(False)
+        self.tabWidget_Ribbon_Bar.addTab(self.tab_1_Home, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget_Ribbon_Bar.addTab(self.tab_2, "")
+
+        self.horizontalLayout_1_Ribbon_Bar.addWidget(self.groupBox_2_Tabs)
+
+        self.horizontalLayout_1_Ribbon_Bar.setStretch(0, 3)
+        self.horizontalLayout_1_Ribbon_Bar.setStretch(1, 15)
+
+        self.verticalLayout_1_Ribbon_Bar.addLayout(self.horizontalLayout_1_Ribbon_Bar)
+
+
+        self.verticalLayout.addLayout(self.verticalLayout_1_Ribbon_Bar)
+
+        self.verticalLayout_2_Main = QVBoxLayout()
+        self.verticalLayout_2_Main.setObjectName(u"verticalLayout_2_Main")
+        self.horizontalLayout_1_Main = QHBoxLayout()
+        self.horizontalLayout_1_Main.setObjectName(u"horizontalLayout_1_Main")
+        self.stackedWidget_Main = QStackedWidget(self.centralwidget)
+        self.stackedWidget_Main.setObjectName(u"stackedWidget_Main")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.stackedWidget_Main.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.stackedWidget_Main.addWidget(self.page_2)
+
+        self.horizontalLayout_1_Main.addWidget(self.stackedWidget_Main)
+
+
+        self.verticalLayout_2_Main.addLayout(self.horizontalLayout_1_Main)
+
+
+        self.verticalLayout.addLayout(self.verticalLayout_2_Main)
+
+        self.verticalLayout.setStretch(0, 2)
+        self.verticalLayout.setStretch(1, 5)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.groupBox_1_Master.setTitle("")
+        self.label_Master_R3C0.setText(QCoreApplication.translate("MainWindow", u"R3C0", None))
+        self.label_Master_R2C0.setText(QCoreApplication.translate("MainWindow", u"R2C0", None))
+        self.label_Master_R0C2.setText(QCoreApplication.translate("MainWindow", u"R0C2", None))
+        self.label_Master_R2C1.setText(QCoreApplication.translate("MainWindow", u"R2C1", None))
+        self.label_Master_R3C1.setText(QCoreApplication.translate("MainWindow", u"R3C1", None))
+        self.label_Master_R1C2.setText(QCoreApplication.translate("MainWindow", u"R1C2", None))
+        self.label_Master_R1C3.setText(QCoreApplication.translate("MainWindow", u"R1C3", None))
+        self.label_Master_R2C2.setText(QCoreApplication.translate("MainWindow", u"R2C2", None))
+        self.label_Master_R2C3.setText(QCoreApplication.translate("MainWindow", u"R2C3", None))
+        self.label_Master_R3C2.setText(QCoreApplication.translate("MainWindow", u"R3C2", None))
+        self.label_Master_R0C0.setText(QCoreApplication.translate("MainWindow", u"R0C0", None))
+        self.pushButton_Master_R0C3.setText(QCoreApplication.translate("MainWindow", u"R0C3", None))
+        self.pushButton_Master_R3C3.setText(QCoreApplication.translate("MainWindow", u"R3C3", None))
+        self.groupBox_2_Tabs.setTitle("")
+        self.groupBox_T1B1.setTitle("")
+        self.label_T1B1.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.groupBox_T1B2.setTitle("")
+        self.tabWidget_Ribbon_Bar.setTabText(self.tabWidget_Ribbon_Bar.indexOf(self.tab_1_Home), QCoreApplication.translate("MainWindow", u"Home", None))
+        self.tabWidget_Ribbon_Bar.setTabText(self.tabWidget_Ribbon_Bar.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
+    # retranslateUi
+
