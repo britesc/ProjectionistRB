c15e2cd6803e6dfb73a172a7bae6a6afd3bb9c61
Julian Bourne <julian@britesc.com>
2023-08-14 13:39:39 +0200

    Created Base Files

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys
+# import qdarktheme
+import traceback
+
+from PySide6.QtWidgets import QApplication
+
+from mainwindow import MainWindow
+
+
+def main() -> None:
+    try:
+        app = QApplication(sys.argv)
+
+        window = MainWindow(app)
+        window.show()
+
+    except Exception as err:
+        print("Unfortunately the Application has encountered an error \
+and is unable to continue.")
+        print(f"Exception {err=}, {type(err)=}")
+        traceback.print_exc()
+        traceback.print_exception() # type: ignore
+
+    finally:
+        sys.exit(app.exec()) # type: ignore
+
+if __name__ == '__main__':
+    main()
---
+++ mainwindow.py
@@ -0,0 +1,49 @@
+import os
+
+from PySide6 import (
+    QtGui,
+    QtCore
+)
+
+from PySide6.QtCore import (
+    QSize,
+    QFileInfo
+)
+    
+from PySide6.QtGui import (
+    # QAction,
+    # QIcon,
+    QPixmap
+)
+    
+from PySide6.QtWidgets import (
+    QMainWindow,
+    # QToolBar,
+    # QPushButton,
+    # QStatusBar,
+    # QApplication,
+    QHeaderView,
+    # QAbstractItemView,
+    QTableWidget,
+    QTableWidgetItem,
+    # QVBoxLayout,
+    # QHBoxLayout,
+    # QTableView,
+    QLabel,
+)    
+
+from PySide6.QtCore import (
+    Qt
+)
+
+from sqlitedict import (
+    SqliteDict
+)
+
+from mainwindow_ui import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, app) -> None:
+        super().__init__()
+        self.setupUi(self) # type: ignore
+        self.app = app #declare an app member
